86fdfd890ba6c01c8a2c316926ec19d8adc42be3
Cleber Couto Filho <clebercoutof@gmail.com>
2018-04-24 17:03:12 -0300

    Change install file

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get install ros-kinetic-velocity-controllers
 sudo apt-get install ros-kinetic-position-controllers
+sudo apt-get install ros-kinetic-effort-controllers
 sudo apt-get install ros-kinetic-gazebo-ros
 sudo apt-get install ros-kinetic-gazebo-ros-control
 sudo apt-get install ros-kinetic-librviz-tutorial
@@ -9,3 +10,5 @@ sudo apt-get install ros-kinetic-xacro
 sudo apt-get install ros-kinetic-ps3joy
 sudo apt-get install ros-kinetic-joint-state-controller
 sudo apt-get install ros-kinetic-joint-state-publisher
+sudo apt-get install ros-kinetic-hector-gazebo-plugins
+sudo apt-get install ros-kinetic-hector-gazebo-thermal-camera
