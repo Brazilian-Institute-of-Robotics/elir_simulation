9d4ce0df1abe5c476adbde3f9e46a90031584bcf
Cleber Couto Filho <clebercoutof@gmail.com>
2018-08-29 13:44:37 -0300

    add dynamixel_driver package install

--- install/install.sh
+++ install/install.sh
@@ -13,3 +13,5 @@ sudo apt-get install ros-kinetic-joint-state-controller
 sudo apt-get install ros-kinetic-joint-state-publisher
 sudo apt-get install ros-kinetic-hector-gazebo-plugins
 sudo apt-get install ros-kinetic-hector-gazebo-thermal-camera
+sudo apt-get install ros-kinetic-dynamixel-driver
+
