6792190831efb179822eb5f8e54ebc3fc0346551
Cleber Couto Filho <clebercoutof@gmail.com>
2018-11-07 17:00:59 -0300

    add line simulation line control package

---
+++ simulation_line_control/src/command_mimic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg  import Float64
+from geometry_msgs.msg import Twist
+
+class line_mimic_control():
+
+    def __init__(self):
+        #Creating our node,publisher and subscriber
+        self.key_subscriber = rospy.Subscriber('/key_vel', Twist, self.callback)
+        self.traction_f1_publisher = rospy.Publisher('elir/traction_f1_controller/command', Float64, queue_size=10)
+        self.traction_f2_publisher = rospy.Publisher('elir/traction_f2_controller/command', Float64, queue_size=10)
+        self.traction_b1_publisher = rospy.Publisher('elir/traction_b1_controller/command', Float64, queue_size=10)
+        self.traction_b2_publisher = rospy.Publisher('elir/traction_b2_controller/command', Float64, queue_size=10)
+        self.traction_ap_publisher = rospy.Publisher('elir/traction_ap_controller/command', Float64, queue_size=10)
+
+        rospy.spin()
+    #Callback function implementing the pose value received
+    def callback(self, data):
+        key_vel = 30*data.angular.z
+        self.traction_f1_publisher.publish(key_vel)
+        self.traction_f2_publisher.publish(-key_vel)
+        self.traction_b1_publisher.publish(-key_vel)
+        self.traction_b2_publisher.publish(key_vel)
+        self.traction_ap_publisher.publish(key_vel)
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('simulation_line_controller', anonymous=True)
+        x = line_mimic_control()
+    except rospy.ROSInterruptException: pass
