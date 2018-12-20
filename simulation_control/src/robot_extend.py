1a004b39846c3dde3a9cdba6b6cf42299941582c
Ícaro Nascimento Queiroz <icaro.nasq@gmail.com>
2018-12-20 01:09:05 -0300

    add comments to robot extend

--- simulation_control/src/robot_extend.py
+++ simulation_control/src/robot_extend.py
@@ -6,7 +6,11 @@ from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 class arm_mimic_control():
-
+"""
+This class is created to configures the subscribers and publisher of joints, teleop keys and traction units, also the parameters
+of the back arm joints and front arm joints and their traction units. It contais the methods to get the joints positions and 
+teleop values to convert and publish on both arms joints and traction units.
+"""
     def __init__(self):
         #Creating our node,publisher and subscriber
         self.key_subscriber = rospy.Subscriber('/key_vel', Twist, self.callback)
@@ -24,10 +28,18 @@ class arm_mimic_control():
 
     #Callback function implementing the pose value received
     def callback_joints(self, msg):
+  	"""
+  	Method to get joints positions 
+	: msg JointState:	Variable with joints positions informations
+  	"""
         self.current_joint1b = msg.position[2]
         self.current_joint1f = msg.position[3] 	
 
     def callback(self, data):
+  	"""
+  	Method to get the value published on key_vel topic, to realize a conversion an publish on joints and traction units
+	:data Float64: 		Value to be converted and published on joints and traction units
+  	"""
         #Verifies if is a null value on angular z
         if data.angular.z == 0:
             pass
@@ -36,52 +48,47 @@ class arm_mimic_control():
             pass
 
         time = rospy.get_time()
-        #Create messages to publish on b_arm joints
-        msg_to_b_arm = JointTrajectory()
-	#Configuring joints of b_arm                    
-        msg_to_b_arm.joint_names = self.b_arm_joints
-	#Configure points to b_arm          
-        msg_to_b_arm.points = []
-        #Configure JointTractoryPoint to b_arm                         
-        points_to_b_arm = JointTrajectoryPoint() 
-        #Configure time of duration for b_arm action           
+	#Create messages to publish on b_arm joints and configures b_arm parameters
+        msg_to_b_arm = JointTrajectory()                   
+        msg_to_b_arm.joint_names = self.b_arm_joints         
+        msg_to_b_arm.points = []                       
+        points_to_b_arm = JointTrajectoryPoint()            
         points_to_b_arm.time_from_start = self.trajectory_duration
-        #Create messages to publish on f_arm joints     
-	msg_to_f_arm = JointTrajectory()     
-	#Configuring joints of f_arm               
-        msg_to_f_arm.joint_names = self.f_arm_joints 
-	#Configure points to f_arm         
-        msg_to_f_arm.points = []
-        #Configure JointTractoryPoint to f_arm                            
-        points_to_f_arm = JointTrajectoryPoint()   
-        #Configure time of duration for f_arm action      
+        #Create messages to publish on f_arm joints and configures f_arm parameters 
+	msg_to_f_arm = JointTrajectory()                   
+        msg_to_f_arm.joint_names = self.f_arm_joints          
+        msg_to_f_arm.points = []                           
+        points_to_f_arm = JointTrajectoryPoint()     
         points_to_f_arm.time_from_start = self.trajectory_duration  
-	#Creating message to publish on joints
-        key_vel = self.current_joint1b + 3 * data.angular.z       
+	#Converts the value of data of key_vel for arms
+        key_vel = self.current_joint1b + 3 * data.angular.z   
+	#Converts the value of data of key_vel for traction units    
         key_vel_to_wheels = 3 * data.angular.z
-	#Points to publish on f_arm
+	#Creates the list of points to publish
         points_to_f_arm.positions = [key_vel, -key_vel]
-	#Points to publish on b_arm
         points_to_b_arm.positions = [key_vel, -key_vel]
-	#Message to publish on b_arm
+	#Update the list of points
         msg_to_b_arm.points.append(points_to_b_arm)
-	#Message to publish on f_arm
         msg_to_f_arm.points.append(points_to_f_arm)
-
         #Verifies the limits of key_vel value
+	#TODO: Adjust limits of key_vel
         if(key_vel >= 3000 or key_vel <= -3000):
 	    #Value to publish don't change	
             key_vel = self.current_joint1b
+	    #Publish key_vel value on joints
             self.b_arm_publisher.publish(msg_to_b_arm)
             self.f_arm_publisher.publish(msg_to_f_arm)
+	    #Publish key_vel_to_wheels value on traction units
             self.traction_f1_publisher.publish(key_vel_to_wheels)
             self.traction_f2_publisher.publish(-key_vel_to_wheels)
             self.traction_b1_publisher.publish(-key_vel_to_wheels)
             self.traction_b2_publisher.publish(key_vel_to_wheels)
 	#Write the values of arm joints        
 	else:
+	    #Publish key_vel value on joints
             self.b_arm_publisher.publish(msg_to_b_arm)
             self.f_arm_publisher.publish(msg_to_f_arm)
+	    #Publish key_vel_to_wheels value on traction units
             self.traction_f1_publisher.publish(key_vel_to_wheels)
             self.traction_f2_publisher.publish(-key_vel_to_wheels)
             self.traction_b1_publisher.publish(-key_vel_to_wheels)
